227fd214504debf9a5036fef35105c63809128c9
Danny <danny.r.huang@gmail.com>
2020-07-22 17:16:48 -0700

    algorithm practice

---
+++ Python/Medium/shiftedBinarySearch.py
@@ -0,0 +1,21 @@
+def shiftedBinarySearch(array, target):
+    # Write your code here.
+  left = 0
+  right = len(array) - 1
+
+  while left <= right:
+      pivot = (left + right) // 2
+      potentialMatch = array[pivot]
+      if potentialMatch == target:
+        return pivot
+      elif potentialMatch >= array[left]:
+        if target < potentialMatch and target >= array[left]:
+          right = pivot - 1
+        else:
+          left = pivot + 1
+      else:
+        if target > potentialMatch and target <= array[right]:
+          left = pivot + 1
+        else:
+          right = pivot - 1
+  return -1
