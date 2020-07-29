02546c8275900a85dd640ef8d477047a17c43406
Danny <danny.r.huang@gmail.com>
2020-07-29 11:55:39 -0700

    algorithm practice

---
+++ Python/Medium/findKClosestElements.py
@@ -0,0 +1,22 @@
+def findClosestElements(arr, k, x):
+  left = 0
+  right = len(arr) - 1
+  index = False
+  
+  while left <= right:
+    pivot = (left + right) // 2
+    potentialMatch = arr[pivot]
+
+    if potentialMatch == x:
+      index = pivot
+    elif potentialMatch > x:
+      left = pivot + 1
+    else:
+      right = pivot - 1
+  
+  
+
+
+
+print(findClosestElements([1,2,3,4,5],4,3))
+print(findClosestElements([1,2,3,4,5],4,-1))
