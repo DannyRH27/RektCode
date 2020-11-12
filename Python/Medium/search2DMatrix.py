a245602e019b9f3ae2c5f2fd43f262a388d79e94
Danny <danny.r.huang@gmail.com>
2020-11-11 17:27:13 -0800

    algorithm practice

--- Python/Medium/search2DMatrix.py
+++ Python/Medium/search2DMatrix.py
@@ -10,3 +10,47 @@ def searchMatrix(matrix, target):
     else:
       return True
   return False
+
+
+def find_in(lst, number):
+    """
+    A function to find a number in a 2D list
+    :param lst: A 2D list of integers
+    :param number: A number to be searched in the 2D list
+    :return: True if the number is found, otherwise False
+    """
+
+    # Write your code here!
+    if len(lst) is 0 or len(lst[0]) is 0:
+        return False
+    start = 0
+    end = len(lst)-1
+
+    while start <= end:
+        row = (start + end) // 2
+        check_start = lst[row][0]
+        check_end = lst[row][-1]
+        if number == check_start or number == check_end:
+            return True
+        if number > check_start and number < check_end:
+            break
+        elif number < check_start and row > 0:
+            end = row - 1
+        elif number > check_end and row < len(lst)-1:
+            start = row + 1
+        else:
+          break
+
+    start = 0
+    end = len(lst[0])-1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if lst[row][mid] == number:
+            return True
+        if lst[row][mid] > number:
+            end = mid - 1
+        if lst[row][mid] < number:
+            start = mid + 1
+
+    return False
