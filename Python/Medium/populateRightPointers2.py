8eeae2a6f556926c88db547fb0829359c786fb59
Danny <danny.r.huang@gmail.com>
2020-12-06 17:22:58 -0800

    algorithm practice

---
+++ Python/Medium/populateRightPointers2.py
@@ -0,0 +1,25 @@
+from collections import deque
+def connect(root):
+  if not root:
+    return None
+
+  q = deque()
+  q.append(root)
+
+  while q:
+    size = len(q)
+
+    for i in range(size):
+      node = q.popleft()
+
+      if i < size - 1:
+        node.next = q[0]
+
+      if node.left:
+        q.append(node.left)
+      
+      if node.right:
+        q.append(node.right)
+  
+  return root
+  
